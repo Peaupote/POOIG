ed4172c150d0d692b13106ba646b2f9eb2c5c4c4
firgaty <firgaty@gmail.com>
2017-12-13 00:51:25 +0100

    Rebased Question class, created more intuitive static methods. QuestionBank WIP

--- src/fr/ip/model/util/Level.java
+++ src/fr/ip/model/util/Level.java
@@ -17,4 +17,51 @@ public enum Level {
     public String toString() {
         return name;
     }
+
+    public static Level levelFromString(String level) {
+        switch (level.toLowerCase()) {
+            case "débutant":
+                return Level.BEGINNER;
+            case "debutant":
+                return Level.BEGINNER;
+            case "intermédiaire":
+                return Level.INTERMEDIATE;
+            case "intermediaire":
+                return Level.INTERMEDIATE;
+            case "confirmé":
+                return Level.EXPERIENCED;
+            case "confirme":
+                return Level.EXPERIENCED;
+            case "expert":
+                return Level.EXPERT;
+            case "mixte":
+                return Level.ANY;
+            default:
+                new Message(MessageType.WARNING, "Niveau de question non spécifié ou incorrect.").print();
+                return null;
+        }
+    }
+
+    public static boolean isLevel(String level) {
+        switch (level.toLowerCase()) {
+            case "débutant":
+                return true;
+            case "debutant":
+                return true;
+            case "intermédiaire":
+                return true;
+            case "intermediaire":
+                return true;
+            case "confirmé":
+                return true;
+            case "confirme":
+                return true;
+            case "expert":
+                return true;
+            case "mixte":
+                return true;
+            default:
+                return false;
+        }
+    }
 }
--- src/fr/ip/model/util/QuestionBank.java
+++ src/fr/ip/model/util/QuestionBank.java
@@ -1,97 +1,27 @@
 package fr.ip.model.util;
 
-import com.sun.xml.internal.ws.api.message.MessageWritable;
-import org.omg.CORBA.Any;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.io.File;
 import java.util.Scanner;
 
 
 public class QuestionBank {
-    public class Question {
-        private final String question;
-        private final Answer answer;
-        private final Level level;
-
-        public Question(Level level, String question, Answer answer) {
-            this.question = question;
-            this.answer = answer;
-            this.level = level;
-        }
-
-        public String getQuestion() {
-            return question;
-        }
-
-        public Answer getAnswer() {
-            return answer;
-        }
-
-        public Level getLevel() {
-            return level;
-        }
-
-        public boolean isLevel(Level l) {
-            return l == level;
-        }
-    }
-
-    public class Answer {
-        String answer;
-
-        public Answer(String answer) {
-            this.answer = answer;
-        }
-        public Answer(int answer) {
-            this.answer = Integer.toString(answer);
-        }
-        public Answer(Answer a) {
-            this.answer = a.getAnswer();
-        }
-
-        public String getAnswer() {
-            return answer;
-        }
-
-        public boolean isCorrect(Answer a) {
-            return a.getAnswer().toLowerCase().equals(answer.toLowerCase());
-        }
-    }
-
-    public class QuestionMC extends Question implements Iterable<Tuple<Answer, String>>{
-        private ArrayList<Tuple<Answer, String>> as;
-
-        public QuestionMC(Level level, String question, ArrayList<Tuple<Answer, String>> as, Answer answer) {
-            super(level, question, answer);
-            this.as = as;
-        }
-
-        public ArrayList<Tuple<Answer, String>> getAnswersArray() {
-            return as;
-        }
-
-        @Override
-        public Iterator<Tuple<Answer, String>> iterator() {
-            return as.iterator();
-        }
-    }
 
     private ArrayList<Question> questions;
     private Level level;
     private File folder;
 
     public QuestionBank(Level level) {
-        if(SETTINGS.instance.isSetting("ANY_LEVEL_OVERRIDE"))
-            this.level = (boolean) SETTINGS.instance.getSetting("ANY_LEVEL_OVERRIDE").value ? Level.ANY : level;
+        SETTINGS settings = new SETTINGS();
+
+        if(settings.isSetting("ANY_LEVEL_OVERRIDE"))
+            this.level = (boolean) settings.getSetting("ANY_LEVEL_OVERRIDE").value() ? Level.ANY : level;
         else {
             System.out.println("");
         }
         try {
-            this.folder = new File((String) SETTINGS.instance.getSetting("PATH_TO_QUESTIONS_FOLDER").value);
+            this.folder = new File((String) settings.getSetting("PATH_TO_QUESTIONS_FOLDER").value());
         } catch (NullPointerException e) {
             new Message(MessageType.ERROR, "Le dossier des questions est introuvable, existe-t-il ?").print();
             System.exit(2);
@@ -113,9 +43,10 @@ public class QuestionBank {
 
         for(File f : fs) {
             parsed = parse(f);
-            if(parsed != null)
+            if(parsed != null) {
                 list.addAll(parsed);
-            parsed = null;
+                parsed = null;
+            }
         }
 
         return list;
@@ -123,43 +54,47 @@ public class QuestionBank {
 
     private ArrayList<Question> parse(File f) {
         Scanner sc;
+        String canonicalPath;
 
         try {
-            new Message(MessageType.INFORMATION, "Opening" + f.getCanonicalPath()).print();
+            canonicalPath = f.getCanonicalPath();
+            new Message(MessageType.INFORMATION, "Opening" + canonicalPath).print();
             sc = new Scanner(f);
         } catch (IOException e) {
             new Message(MessageType.ERROR, "Wrong file path").print();
-        } catch (FileNotFoundException e) {
-            try {
-                new Message(MessageType.WARNING, "File" + f.getCanonicalPath() + "not found").print();
-            } catch (IOException err) {
-                new Message(MessageType.ERROR, "Wrong file path").print();
             return null;
         }
 
         ArrayList<Question> qs = new ArrayList<>();
         Scanner line;
         String sLine;
-        Question q;
+        Question q = null;
         boolean valid = true;
-        int questionType = -1;
+        Question.QuestionType questionType;
+        int lineNb = 0;
+
+        questionType = parseQuestionType(sc, lineNb, canonicalPath);
+        if(questionType == Question.QuestionType.NONE)
+            return null;
 
         while(sc.hasNextLine()) {
-            while(questionType == -1) {
-                
-            }
+
 
             sLine = sc.nextLine();
+
             if(!valid && StringUtil.isEmpty(sLine))
                 valid = true;
             else
                 continue;
             if(StringUtil.removeWhiteSpace(sLine).startsWith("**"))
                 continue; // Checks if comment line.
-            if(StringUtil.removeWhiteSpace(sLine).startsWith("[") && !isLevel(levelFromLine(sLine))) {
+            if(StringUtil.removeWhiteSpace(sLine).startsWith("[") && !isRightLevel(levelFromLine(sLine))) {
                 valid = false;
                 continue;
             }
+
+
+
             if(q != null)
                 qs.add(q);
 
@@ -168,39 +103,42 @@ public class QuestionBank {
         return qs;
     }
 
+    private Question.QuestionType parseQuestionType(Scanner sc, int lineNb, String path) {
+        Scanner line;
+        String sLine;
+        Question.QuestionType questionType;
+
+        while(sc.hasNextLine()) {
+            sLine = sc.nextLine();
+            lineNb ++;
+            if(StringUtil.removeWhiteSpace(sLine).startsWith("!!")) {
+                line = new Scanner(StringUtil.removeWhiteSpace(sLine));
+                line.useDelimiter("!!");
+                questionType = Question.QuestionType.questionTypeFromString(line.next());
+
+                if(questionType != Question.QuestionType.NONE) {
+                    new Message(MessageType.INFORMATION, path + " : " + questionType.toString() + " questions file.").print();
+                    return questionType;
+                } else
+                    new Message(MessageType.WARNING, "Question type not recognized at line " + lineNb + ".").print();
+            }
+        }
+
+        new Message(MessageType.ERROR, path + " : No question type for file.").print();
+        return Question.QuestionType.NONE;
+    }
+
     private Level levelFromLine(String line) {
         String sLevel = StringUtil.removeWhiteSpace(line);
         sLevel = sLevel.substring(1, sLevel.length() - 2);
 
         new Message(MessageType.INFORMATION, "Ligne de niveau minimisée: " + sLevel).print();
 
-        Level out;
-
-        switch (sLevel) {
-            case "Débutant":
-                out = Level.BEGINNER;
-                break;
-            case "Intermédiaire":
-                out = Level.INTERMEDIATE;
-                break;
-            case "Confirmé":
-                out = Level.EXPERIENCED;
-                break;
-            case "Expert":
-                out = Level.EXPERT;
-                break;
-            case "Mixte":
-                out = Level.ANY;
-                break;
-            default:
-                new Message(MessageType.WARNING, "Niveau de question non spécifié ou incorrect.").print();
-                out = null;
-        }
-
-        return out;
+        return Level.levelFromString(sLevel);
     }
 
-    public boolean isLevel(Level l) {
+    public boolean isRightLevel(Level l) {
+        if(l == null) return false;
         if(this.level == Level.ANY || l == Level.ANY) return true;
         return this.level == l;
     }
