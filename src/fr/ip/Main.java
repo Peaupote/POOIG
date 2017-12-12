75fc0a04fb1df64ad7d1994304bf3f4523bed4e9
firgaty <firgaty@gmail.com>
2017-12-13 00:50:14 +0100

    Settings operational and finished

--- src/fr/ip/Main.java
+++ src/fr/ip/Main.java
@@ -13,18 +13,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Game g;
+        SETTINGS s = new SETTINGS();
 
-        System.out.println("1 : Goose\n2 : Numeri\n3 : GUI test\n4 : SETTINGS test\n0 : Exit");
+        System.out.println("1 : Goose\n2 : Numeri\n3 : GUI test\n4 : Question test\n0 : Exit");
 
         switch (sc.nextInt()) {
             case 0 : System.exit(0); return;
             case 1 : g = new GooseGame(); break;
             case 2 : g = new NumeriGame(); break;
             case 3 : new MainFrame(); return;
-            case 4 : new SETTINGS(); g = new GooseGame(); break;
+            case 4 : g = new GooseGame();
+                break;
             default: System.exit(1); return;
         }
 
         g.play();
+
+        s.write();
+
     }
 }
